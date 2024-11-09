ae4f0d867875c863bcb1fecd33baef6cfe57e8ec
GabrielRosa <git@secondmind.com.br>
2024-11-09 20:15:09 -0300

    #feat- header delay

--- src/app/components/Projects/index.tsx
+++ src/app/components/Projects/index.tsx
@@ -1,15 +1,34 @@
 'use client'
-import React from 'react';
+import React, { useEffect } from 'react';
 // import { Container } from './styles';
 import styles from './styles.module.css'
+
+import Brain from '../../../assets/images/cerebro.png'
+import Image from 'next/image';
+import { useWindowScroll } from '@/app/customHook/useWindoSrool';
 const Projects: React.FC = () => {
+    const scroll = useWindowScroll()
+
+    useEffect(() => {
+        const headerdata = document.querySelector('#header') 
+        if (scroll > 1762) {
+           if(headerdata)
+            headerdata.className = `${styles.header} animate__animated animate__jackInTheBox`;
+        }else {
+            if(headerdata) {
+                headerdata.className = `${styles.header}`
+            }
+        }
+    }, [scroll]);
     return (
         <section className={styles.container}>
-            <article className={styles.header}>
-                <h2>Projetos Principais</h2>
-                <button>
-                    <p>Ver Tudo</p>
-                </button>
+            <article className={`${styles.header}`} id='header' >
+                <h2>Desenvolvendo software sob medida que faz a diferença</h2>
+                <p>
+                    Se você precisa de um software personalizado e que realmente funcione, estamos prontos para ajudar!
+                </p>
+                <button>Pedir Orçamento</button>
+                <Image src={Brain} alt='Brain' className={styles.imageBrain} />
             </article>
         </section>
     );
